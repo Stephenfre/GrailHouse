e52cd114b8d497011a73b5f56fae0e9e8d32f57b
Stephenfre <stephen.freeman.dev@gmail.com>
2021-08-17 01:58:43 -0700

    Styling: mobile nav bar

--- src/features/Nav/Links/YeezyLinks.js
+++ src/features/Nav/Links/YeezyLinks.js
@@ -1,28 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-import { makeStyles } from "@material-ui/core/styles";
-import List from "@material-ui/core/List";
-import ListItem from "@material-ui/core/ListItem";
-import ListItemText from "@material-ui/core/ListItemText";
-
-const useStyles = makeStyles((theme) => ({
-    root: {
-        width: "100%",
-        display: "flex",
-        alignItems: "center",
-        height: "13rem",
-        backgroundColor: theme.palette.background.paper,
-    },
-}));
-
-function ListItemLink(props) {
-    return <ListItem button component={Link} {...props} />;
-}
-
 export default function YeezyLinks() {
-    const classes = useStyles();
-
     return (
         <div className="shoe-links-mobile-list">
             <ul component="all-links-list" style={{ width: "50%" }}>
