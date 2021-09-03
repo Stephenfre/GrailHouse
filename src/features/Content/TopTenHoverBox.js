0d05ecf7db841f51e254b6f521e74effa9324fca
Stephenfre <stephen.freeman.dev@gmail.com>
2021-09-03 02:08:11 -0700

    Fixed: hover box css

--- src/features/Content/TopTenHoverBox.js
+++ src/features/Content/TopTenHoverBox.js
@@ -5,11 +5,11 @@ import "./HomePage/HomePage.css";
 
 const HoverDiv = styled.div`
     width: 20%;
-    height: 13%;
-    zindex: 9999;
+    height: 10%;
+    z-index: 9999;
     position: absolute;
-    bottom: 20.5rem;
-    left: 25rem;
+    bottom: 27.5rem;
+    left: 27rem;
     background: white;
     @media (max-width: 600px) {
         width: 60%;
