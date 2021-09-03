e25c2d9e147dd07635def7223425a9aa1938e054
Stephenfre <stephen.freeman.dev@gmail.com>
2021-09-03 01:11:32 -0700

    Fixed: media-query css fixes

--- src/features/Content/Skeletons/HomeSkeletonCards.js
+++ src/features/Content/Skeletons/HomeSkeletonCards.js
@@ -1,60 +1,60 @@
 import React from "react";
 import Skeleton from "react-loading-skeleton";
-import "./Skeleton.css";
+import "./HomeSkeletonCards.css";
 
 export default function HomeSkeletonCards() {
-	return (
-		<React.Fragment>
-			<div className="skeleton-wrap-home">
-				{Array(8)
-					.fill()
-					.map((item, i) => (
-						<div className="skeleton-home">
-							<div className="skeleton-content-home">
-								<div className="skeleton-img-home">
-									<Skeleton width={`100%`} height={`100%`} />
-								</div>
-								<div className="skeleton-details-wrap-home">
-									<div className="skeleton-details-home">
-										<Skeleton width={`100%`} height={`69%`} style={{ marginTop: 10 }} />
-										<div className="skeleton-prices-home">
-											<Skeleton width={`40%`} />
-											<Skeleton width={`25%`} height={30} />
-										</div>
-										<div className="skeleton-got-them-btn-home">
-											<Skeleton width={`100%`} height={`100%`} />
-										</div>
-									</div>
-								</div>
-							</div>
-						</div>
-					))}
-			</div>
-			<div className="skeleton-wrap-mobile-home">
-				{Array(8)
-					.fill()
-					.map((item, i) => (
-						<div className="skeleton-home">
-							<div className="skeleton-content-home">
-								<div className="skeleton-img-home">
-									<Skeleton width={`100%`} height={`100%`} />
-								</div>
-								<div className="skeleton-details-wrap-home">
-									<div className="skeleton-details-home">
-										<Skeleton width={`100%`} height={`69%`} style={{ marginTop: 10 }} />
-										<div className="skeleton-prices-home">
-											<Skeleton width={`40%`} />
-											<Skeleton width={`25%`} height={30} />
-										</div>
-										<div className="skeleton-got-them-btn-home">
-											<Skeleton width={`100%`} height={`100%`} />
-										</div>
-									</div>
-								</div>
-							</div>
-						</div>
-					))}
-			</div>
-		</React.Fragment>
-	);
+    return (
+        <React.Fragment>
+            <div className="skeleton-wrap-home">
+                {Array(8)
+                    .fill()
+                    .map((item, i) => (
+                        <div className="skeleton-home">
+                            <div className="skeleton-content-home">
+                                <div className="skeleton-img-home">
+                                    <Skeleton width={`100%`} height={`100%`} />
+                                </div>
+                                <div className="skeleton-details-wrap-home">
+                                    <div className="skeleton-details-home">
+                                        <Skeleton width={`100%`} height={`69%`} style={{ marginTop: 10 }} />
+                                        <div className="skeleton-prices-home">
+                                            <Skeleton width={`40%`} />
+                                            <Skeleton width={`25%`} height={30} />
+                                        </div>
+                                        <div className="skeleton-got-them-btn-home">
+                                            <Skeleton width={`100%`} height={`100%`} />
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    ))}
+            </div>
+            <div className="skeleton-wrap-mobile-home">
+                {Array(8)
+                    .fill()
+                    .map((item, i) => (
+                        <div className="skeleton-home">
+                            <div className="skeleton-content-home">
+                                <div className="skeleton-img-home">
+                                    <Skeleton width={`100%`} height={`100%`} />
+                                </div>
+                                <div className="skeleton-details-wrap-home">
+                                    <div className="skeleton-details-home">
+                                        <Skeleton width={`100%`} height={`69%`} style={{ marginTop: 10 }} />
+                                        <div className="skeleton-prices-home">
+                                            <Skeleton width={`40%`} />
+                                            <Skeleton width={`25%`} height={30} />
+                                        </div>
+                                        <div className="skeleton-got-them-btn-home">
+                                            <Skeleton width={`100%`} height={`100%`} />
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    ))}
+            </div>
+        </React.Fragment>
+    );
 }
